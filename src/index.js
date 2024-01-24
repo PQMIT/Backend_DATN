1a7d84203db4221b67e951c808c5cebaa0b5c7a5
PQMIT <PQM2112000@gmail.com>
2024-01-25 02:58:56 +0700

    https

--- src/index.js
+++ src/index.js
@@ -25,7 +25,6 @@ app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(morgan('tiny'));
-// app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(camelCaseReq);
@@ -35,23 +34,23 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 require('./routes')(app);
 // Đọc chứng chỉ SSL và khóa riêng tư
-// const privateKey = fs.readFileSync(
-//   '/home/ec2-user/multichoice-api/server-key.pem',
-//   'utf8',
-// );
-// const certificate = fs.readFileSync(
-//   '/home/ec2-user/multichoice-api/server-cert.pem',
-//   'utf8',
-// );
-// const credentials = { key: privateKey, cert: certificate };
+const privateKey = fs.readFileSync(
+  '/etc/letsencrypt/live/multichoice.site/privkey.pem',
+  'utf8',
+);
+const certificate = fs.readFileSync(
+  '/etc/letsencrypt/live/multichoice.site/fullchain.pem',
+  'utf8',
+);
+const credentials = { key: privateKey, cert: certificate };
 
 app.use(errorHandler);
 // Tạo máy chủ HTTPS sử dụng Express
-// const httpsServer = https.createServer(credentials, app);
+const httpsServer = https.createServer(credentials, app);
 
-// httpsServer.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-app.listen(PORT, () => {
+httpsServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+/* app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); */
